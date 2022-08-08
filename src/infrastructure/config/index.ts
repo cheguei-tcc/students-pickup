63f9a77d242149a7528d328f9137bb7b30ce4762
iamseki <chrisyuri_19@hotmail.com>
2022-08-08 08:08:26 -0300

    chore: add envs to deploy stack

--- src/infrastructure/config/index.ts
+++ src/infrastructure/config/index.ts
@@ -9,7 +9,7 @@ const configFromEnv = (): Config => ({
   nodeEnv: process.env.NODE_ENV || 'local',
   logLevel: process.env.LOG_LEVEL || 'info',
   port: process.env.PORT || '3000',
-  queue: process.env.QUEUE || ''
+  queue: process.env.QUEUE || 'sqs-local'
 });
 
 export { Config, configFromEnv };
